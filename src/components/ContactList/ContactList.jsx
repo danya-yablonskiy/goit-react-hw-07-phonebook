8fe77d11ca8726f942ad373b70c79c9e8bbf94fe
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-15 18:33:05 +0300

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { ListItem, Button, List } from './ContactList.styled';
+
+export const ContactList = () => {
+  const {
+    contacts: {
+      contacts: { items },
+      filter,
+    },
+  } = useSelector(state => state);
+
+  // const dispatch = useDispatch();
+  // const filterName = items.filter(item =>
+  //   item.name.toLowerCase().includes(filter.toLowerCase())
+  // );
+
+  // const deleteContact = id => {
+  //   dispatch(deleteContacts(id));
+  // };
+
+  return (
+    <List>
+      {/* {filterName.map(item => (
+        <ListItem key={item.id}>
+          <p>
+            {item.name}: {item.number}
+          </p>
+          <Button type="button" onClick={() => deleteContact(item.id)}>
+            Delete
+          </Button>
+        </ListItem>
+      ))} */}
+    </List>
+  );
+};
