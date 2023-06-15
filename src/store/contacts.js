c9001c8de1444450bc4e0bc68a8952edb37bf1c0
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-15 18:59:31 +0300

    Update contacts.js

--- src/store/contacts.js
+++ src/store/contacts.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getContacts } from './API/getContacts';
 
 const initialState = {
@@ -10,35 +10,57 @@ const initialState = {
   filter: '',
 };
 
-export const getContactsAction = () => {
-  return async dispatch => {
-    try {
-      dispatch(contactsSlice.actions.fetching());
-      const data = await getContacts();
-      dispatch(contactsSlice.actions.fetchingSuccess(data));
-    } catch (error) {
-      dispatch(contactsSlice.actions.fetchingError(error));
-    }
-  };
-};
+// export const getContactsAction = () => {
+//   return async dispatch => {
+//     try {
+//       dispatch(contactsSlice.actions.fetching());
+//       const data = await getContacts();
+//       dispatch(contactsSlice.actions.fetchingSuccess(data));
+//     } catch (error) {
+//       dispatch(contactsSlice.actions.fetchingError(error));
+//     }
+//   };
+// };
+
+export const getContactsThunk = createAsyncThunk(
+  'contacts/getContacts/pending',
+  async () => {
+    return await getContacts();
+  }
+);
 
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-  reducers: {
-    fetching: state => {
-      state.contacts.isLoading = true;
+//   extraReducers: builder => {
+//     builder
+//       .addCase(getContactsThunk.pending, state => {
+//         state.contacts.isLoading = true;
+//       })
+//       .addCase(getContactsThunk.fulfilled, (state, { paylaod }) => {
+//         state.contacts.isLoading = false;
+//         state.contacts.error = '';
+//         state.contacts.items = paylaod;
+//       })
+//       .addCase(getContactsThunk.rejected, (state, { paylaod }) => {
+//         state.contacts.isLoading = false;
+//         state.contacts.error = paylaod;
+//       });
+//   },
+    reducers: {
+      fetching: state => {
+        state.contacts.isLoading = true;
+      },
+      fetchingSuccess: (state, { paylaod }) => {
+        state.contacts.isLoading = false;
+        state.contacts.error = '';
+        state.contacts.items = paylaod;
+      },
+      fetchingError: (state, { paylaod }) => {
+        state.contacts.isLoading = false;
+        state.contacts.error = paylaod;
+      },
     },
-    fetchingSuccess: (state, { paylaod }) => {
-      state.contacts.isLoading = false;
-      state.contacts.error = '';
-      state.contacts.items = paylaod;
-    },
-    fetchingError: (state, { paylaod }) => {
-      state.contacts.isLoading = false;
-      state.contacts.error = paylaod;
-    },
-  },
 });
 
 export const contactsReducer = contactsSlice.reducer;
