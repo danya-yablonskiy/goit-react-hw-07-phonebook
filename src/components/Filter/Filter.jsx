dad616e375d1517fc44d082070039f8fe03c6b7e
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-15 18:33:09 +0300

    Create Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+
+export const Filter = () => {
+  const {
+    contacts: { filter },
+  } = useSelector(state => state);
+  // const dispatch = useDispatch();
+
+  const changeFilter = e => {
+    // dispatch(setFilter(e.currentTarget.value));
+  };
+
+  return (
+    <label>
+      Find contact by name <br />
+      <input type="text" value={filter} onChange={changeFilter}></input>
+    </label>
+  );
+};
