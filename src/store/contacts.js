dfebc3b9ffef4016d0db4dad4af93fb55758860d
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-15 19:06:29 +0300

    Update contacts.js

--- src/store/contacts.js
+++ src/store/contacts.js
@@ -10,57 +10,58 @@ const initialState = {
   filter: '',
 };
 
-// export const getContactsAction = () => {
-//   return async dispatch => {
-//     try {
-//       dispatch(contactsSlice.actions.fetching());
-//       const data = await getContacts();
-//       dispatch(contactsSlice.actions.fetchingSuccess(data));
-//     } catch (error) {
-//       dispatch(contactsSlice.actions.fetchingError(error));
-//     }
-//   };
-// };
+export const getContactsAction = () => {
+  return async dispatch => {
+    try {
+      dispatch(contactsSlice.actions.fetching());
+      const data = await getContacts();
 
-export const getContactsThunk = createAsyncThunk(
-  'contacts/getContacts/pending',
-  async () => {
-    return await getContacts();
-  }
-);
+      dispatch(contactsSlice.actions.fetchingSuccess(data));
+    } catch (error) {
+      dispatch(contactsSlice.actions.fetchingError(error));
+    }
+  };
+};
+
+// export const getContactsThunk = createAsyncThunk(
+//   'contacts/getContacts/pending',
+//   async () => {
+//     return await getContacts();
+//   }
+// );
 
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-//   extraReducers: builder => {
-//     builder
-//       .addCase(getContactsThunk.pending, state => {
-//         state.contacts.isLoading = true;
-//       })
-//       .addCase(getContactsThunk.fulfilled, (state, { paylaod }) => {
-//         state.contacts.isLoading = false;
-//         state.contacts.error = '';
-//         state.contacts.items = paylaod;
-//       })
-//       .addCase(getContactsThunk.rejected, (state, { paylaod }) => {
-//         state.contacts.isLoading = false;
-//         state.contacts.error = paylaod;
-//       });
-//   },
-    reducers: {
-      fetching: state => {
-        state.contacts.isLoading = true;
-      },
-      fetchingSuccess: (state, { paylaod }) => {
-        state.contacts.isLoading = false;
-        state.contacts.error = '';
-        state.contacts.items = paylaod;
-      },
-      fetchingError: (state, { paylaod }) => {
-        state.contacts.isLoading = false;
-        state.contacts.error = paylaod;
-      },
+  //   extraReducers: builder => {
+  //     builder
+  //       .addCase(getContactsThunk.pending, state => {
+  //         state.contacts.isLoading = true;
+  //       })
+  //       .addCase(getContactsThunk.fulfilled, (state, { paylaod }) => {
+  //         state.contacts.isLoading = false;
+  //         state.contacts.error = '';
+  //         state.contacts.items = paylaod;
+  //       })
+  //       .addCase(getContactsThunk.rejected, (state, { paylaod }) => {
+  //         state.contacts.isLoading = false;
+  //         state.contacts.error = paylaod;
+  //       });
+  //   },
+  reducers: {
+    fetching: state => {
+      state.contacts.isLoading = true;
     },
+    fetchingSuccess: (state, { paylaod }) => {
+      state.contacts.isLoading = false;
+      state.contacts.error = '';
+      state.contacts.items = paylaod;
+    },
+    fetchingError: (state, { paylaod }) => {
+      state.contacts.isLoading = false;
+      state.contacts.error = paylaod;
+    },
+  },
 });
 
 export const contactsReducer = contactsSlice.reducer;
