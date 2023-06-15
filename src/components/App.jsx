2a90cee1548271755da6e757d9ea739005b7989f
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-15 18:33:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+
+import { Form } from './Form/Form';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { getContactsAction } from 'store/contacts';
+import { useDispatch } from 'react-redux';
+
 export const App = () => {
+  const dispatch = useDispatch()
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      <button onClick={() => dispatch(getContactsAction())}>Test</button>
     </div>
   );
 };
