ace6e39020beb8108af536c25dc689107fd42cf0
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-17 02:50:49 +0300

    Update deleteContacts.js

--- src/store/API/deleteContacts.js
+++ src/store/API/deleteContacts.js
@@ -4,5 +4,6 @@ export const deleteContacts = async contactId => {
   const { data } = await axios.delete(
     `https://648b172d17f1536d65ea553f.mockapi.io/api/v1/contacts/${contactId}`
   );
+
   return data;
 };
