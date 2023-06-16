7a845135b2bcc2b7104dad81651d0bdad4538d13
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-16 17:01:42 +0300

    Update filterSlice.js

--- src/store/filterSlice.js
+++ src/store/filterSlice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { initialState } from './initialState';
 
+const initialState = '';
 const filterSlice = createSlice({
   name: 'filter',
   initialState,
   reducers: {
     setFilter: (state, { payload }) => {
-      return (state.filter = payload);
+      return (state = payload);
     },
   },
 });
