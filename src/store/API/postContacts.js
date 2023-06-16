bb24f7ebd9928641caf7da912bdc0b098bb57d77
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-17 02:50:51 +0300

    Update postContacts.js

--- src/store/API/postContacts.js
+++ src/store/API/postContacts.js
@@ -4,5 +4,6 @@ export const postContacts = async (newContact) => {
   const { data } = await axios.post(
     'https://648b172d17f1536d65ea553f.mockapi.io/api/v1/contacts', newContact
   );
+  console.log(data);
   return data;
 };
