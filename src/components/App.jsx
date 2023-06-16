7d12f28ca1129ba4155e81beafd11d05108f846b
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-16 14:53:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
-
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-import { getContactsAction } from 'store/contacts';
+
 import { useDispatch } from 'react-redux';
 
+import { useEffect } from 'react';
+import { getContactsThunk } from 'store/options';
+
 export const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -14,7 +21,6 @@ export const App = () => {
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
-      <button onClick={() => dispatch(getContactsAction())}>Test</button>
     </div>
   );
 };
